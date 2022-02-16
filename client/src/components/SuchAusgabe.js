098a7f807aedfc4751c27de487fbe85d5de4c6e5
ruuiisu <luis.gjokaj@gmail.com>
2022-02-16 15:25:16 +0100

    Dynamic adding of sitzungszimmer
    falls im json file sitzungszimmer hinzugefuegt werden, werden diese automatisch im react angezeigt.

--- client/src/components/SuchAusgabe.js
+++ client/src/components/SuchAusgabe.js
@@ -2,13 +2,10 @@ import React, {useState, useEffect, useDebugValue} from 'react'
 import axios from 'axios'
 import Zimmer from './requirements/zimmer'
 
-
 const port = 5001;
 const SuchAusgabe = () => {
-
   const [standOrte, setStandOrte] = useState([])
-  const [stockwerke, setStockwerke] = useState([])
-  const [sitzungszimmer, setSitzungszimmer] = useState([])
+  const [sitzungsZimmer, setSitzungsZimmer] = useState([])
 
   useEffect(() => {
     axios.get(`http://localhost:${port}/standorte/`).then((response) => {
@@ -19,21 +16,13 @@ const SuchAusgabe = () => {
     axios.get(`http://localhost:${port}/sitzungszimmer/`).then((response) => {
       setSitzungsZimmer(response.data)   
     })
-  })
-useEffect(() => {
-  axios.get("http://localhost:3001/sitzungszimmer/").then((response) => {
-    setSitzungszimmer(response.data)
-  })
-
-})
-
-
+  }, [])
+console.log(sitzungsZimmer)
   return <div className='Such-Ausgabe'>
-    <div className='standort-test'> 
-      <div>Zimmername: {sitzungsZimmer.filter(v => v.id === 1)[0]?.zimmerName}</div>
-  </div>
-      
-  </div>
+    <div className='standort-test'>
+      <div>{sitzungsZimmer.map( zimmer => <Zimmer zimmername={zimmer.zimmerName} standort={zimmer.standortName} stockwerk={zimmer.stockwerk} maxP={zimmer.maxPersonen} />)}</div>
+    </div>
+  </div>;
 };
 
 export default SuchAusgabe;
