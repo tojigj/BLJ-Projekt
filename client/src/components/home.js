a2014bbf22037639ba029009119fe127155ef65c
ruuiisu <luis.gjokaj@gmail.com>
2022-03-23 13:11:33 +0100

    Filter works
    Filter works with stockwerke, personen and standorte. double filter can be used, but still a bug if deleting filters.

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,6 +7,8 @@ const port = 5001;
 const Home = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [sitzungsZimmer, setSitzungsZimmer] = useState([]);
+  const [shownData, setShownData] = useState([]);
+  let filters = [];
 
   useEffect(() => {
     axios.get(`http://localhost:${port}/sitzungszimmer/`).then((response) => {
@@ -15,22 +17,61 @@ const Home = () => {
     });
   }, []);
 
+  const handleFilterPersonen = (anzPersonen) => {
+    setShownData(filteredData);
+    let filteredPersonen;
+    if (!anzPersonen) {
+      filteredPersonen = shownData;
+      setFilteredData(filteredPersonen);
+      if (filters.includes("standort")) filters -= "person";
+      return;
+    } else {
+      filteredPersonen = filteredData.filter((item) => {
+        return item.maxPersonen >= anzPersonen;
+      });
+    }
+    filters += "person";
+    setFilteredData(filteredPersonen);
+  };
+
   const generateStockwerkData = () => {
     return [...new Set(sitzungsZimmer.map((item) => item.stockwerk))];
   };
 
-  const handleFilterPersonen = (anzPersonen) => {
-    const filteredPersonen = sitzungsZimmer.filter((item) => {
-      return item.maxPersonen >= anzPersonen;
-    });
-    setFilteredData(filteredPersonen);
+  const handleFilterStockwerke = (stockwerk) => {
+    setShownData(filteredData);
+    let filteredStockwerk;
+    if (stockwerk === "") {
+      filteredStockwerk = shownData;
+      setFilteredData(filteredStockwerk);
+      if (filters.includes("standort")) filters -= "stockwerk";
+      return;
+    } else {
+      filteredStockwerk = filteredData.filter((item) => {
+        return item.stockwerk == stockwerk;
+      });
+    }
+    filters += "stockwerk";
+    setFilteredData(filteredStockwerk);
   };
 
-  const handleFilterStockwerke = (stockwerk) => {
-    const filteredStockwerk = sitzungsZimmer.filter((item) => {
-      return item.stockwerk === stockwerk;
-    });
-    setFilteredData(filteredStockwerk);
+  const handleFilterStandorte = (standort) => {
+    setShownData(filteredData);
+    let filteredStandorte;
+    console.log(standort);
+    if (!standort.length) {
+      filteredStandorte = shownData;
+      setFilteredData(filteredStandorte);
+      if (filters.includes("standort")) filters -= "standort";
+      return;
+    } else if (standort.length === 2) filteredStandorte = sitzungsZimmer;
+    else {
+      filteredStandorte = filteredData.filter((item) => {
+        return item.standortName == standort[0].name;
+      });
+    }
+    filters += "standort";
+    setFilteredData(filteredStandorte);
   };
 
   return (
@@ -39,6 +80,7 @@ const Home = () => {
         stockwerke={generateStockwerkData()}
         onStockwerkChange={handleFilterStockwerke}
         onPersonenChange={handleFilterPersonen}
+        onStandortChange={handleFilterStandorte}
       />
       <div className="Such-Ausgabe">
         <div className="standort-test">
