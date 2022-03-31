1a2587de0cfc73816d367e12001fe2feb9d5eb57
AleFacal <75016125+AleFacal@users.noreply.github.com>
2022-03-31 09:31:52 +0200

    Deleted dropdown file

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/header";
 import Home from "./components/home";
 import "./index.css";
 import Footer from "./components/footer";
-import { Dropdown } from "react-bootstrap";
 
 function App() {
   return (
