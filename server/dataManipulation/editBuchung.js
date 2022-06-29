91bcf2c23b319a92e6dfd7314f07829ee985dbea
luis <83016329+tojigj@users.noreply.github.com>
2022-06-29 11:28:35 +0200

    d

--- server/dataManipulation/editBuchung.js
+++ server/dataManipulation/editBuchung.js
@@ -1,5 +1,9 @@
 import { getData } from "../dbAccess.js";
 import { writeDbData } from "../dbAccess.js";
+import Moment from "moment";
+import extendMoment from "moment-range";
+
+const moment = extendMoment.extendMoment(Moment);
 
 //Validierung
 export function editBuchung(
@@ -10,16 +14,35 @@ export function editBuchung(
   zimmerName
 ) {
   const data = getData();
+  let appointmentEdit = false;
   data.Sitzungszimmer.map((item) => {
     if (item.zimmerName) {
       if (item.zimmerName === zimmerName) {
+        const inputRange = moment.range(
+          new Date(newStartDate),
+          new Date(newEndDate)
+        );
         for (let appointment of item.appointments) {
           if (
             appointment.startDate == oldStartDate &&
             appointment.endDate == oldEndDate
           ) {
-            appointment.startDate = newStartDate;
-            appointment.endDate = newEndDate;
+            for (let appoint of item.appointments) {
+              let dbRange = moment.range(
+                new Date(appoint.startDate),
+                new Date(appoint.endDate)
+              );
+              if (dbRange.overlaps(inputRange)) {
+                appointmentEdit = false;
+                break;
+              } else {
+                appointmentEdit = true;
+              }
+            }
+            if (appointmentEdit) {
+              appointment.startDate = newStartDate;
+              appointment.endDate = newEndDate;
+            }
           }
         }
       }
